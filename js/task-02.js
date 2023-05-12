9204cd07a0324d57c2835ab057002647150130aa
foton-1 <ovf0675@outlook.com>
2023-05-12 23:31:00 +0300

    mod-6_task-2
    add task-2 вараіанти

--- js/task-02.js
+++ js/task-02.js
@@ -6,18 +6,41 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-
-const ingradientsElem = [];
-for (const ingrad of ingredients) {
-  const liEl = document.createElement('li');
-  liEl.textContent = ingrad;
-  liEl.classList.add('item');
-  ingradientsElem.push(liEl);
-}
-console.log(ingradientsElem);
-
+/*--------через функцію ---*/
+const makeIngrfdients = parameters => {
+  return parameters.map(ingrad => {
+    const liEl = document.createElement('li');
+    liEl.textContent = ingrad;
+    liEl.classList.add('item');
+    return liEl;
+  });
+};
+const ingradientsElem = makeIngrfdients(ingredients);
 const ulId = document.querySelector('#ingredients');
 ulId.append(...ingradientsElem);
+
+/*--------через map() ---*/
+// const ingradientsElem = ingredients.map(ingrad => {
+//   const liEl = document.createElement('li');
+//   liEl.textContent = ingrad;
+//   liEl.classList.add('item');
+//   return liEl;
+// });
+// console.log(ingradientsElem);
+// const ulId = document.querySelector('#ingredients');
+// ulId.append(...ingradientsElem);
+
+/*--------через for of ---*/
+// const ingradientsElem = [];
+// for (const ingrad of ingredients) {
+//   const liEl = document.createElement('li');
+//   liEl.textContent = ingrad;
+//   liEl.classList.add('item');
+//   ingradientsElem.push(liEl);
+// }
+// console.log(ingradientsElem);
+// const ulId = document.querySelector('#ingredients');
+// ulId.append(...ingradientsElem);
 
 /*--------для одного елемента ---*/
 // const liEl = document.createElement('li');
