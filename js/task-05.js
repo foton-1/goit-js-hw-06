5acc683a575897a008ecfd2afabdfe4cf686158f
foton-1 <ovf0675@outlook.com>
2023-05-14 21:09:50 +0300

    mod-6_task-4
    add task-4

---
+++ js/task-05.js
@@ -0,0 +1,5 @@
+const textInput = document.querySelector('#name-input');
+const outputField = document.querySelector('#name-output');
+textInput.addEventListener('input', event => {
+  outputField;
+});
